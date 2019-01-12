55addcd1f6f9be38f854c22db4560b4a1eae5718
Marc Stirner <marc.stirner@outlook.com>
2019-01-12 22:12:41 +0100

    init

--- services/service.updater.js
+++ services/service.updater.js
@@ -1,1 +1,7 @@
-console.log("Updater")
+const config = require("../config.json").services.dhcp;
+const log = require("../lib/lib.logger.js")("UPDATER");
+const queue = require("../lib/lib.queue.js");
+
+// connect to server
+queue.connect();
+
